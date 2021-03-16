2f538c78874ab5814f06fa82737d8c4630f49dc9
Artur Peplinski <artur.peplinski@netguru.com>
2021-03-16 16:02:35 +0100

    Imported RxCocoa in APIClient and updated send method

--- Networking with RxSwift/APIClient.swift
+++ Networking with RxSwift/APIClient.swift
@@ -1,26 +1,15 @@
 import Foundation
 import RxSwift
+import RxCocoa
 
 class APIClient {
     private let baseURL = URL(string: "http://universities.hipolabs.com/")!
 
     func send<T: Codable>(apiRequest: APIRequest) -> Observable<T> {
-        return Observable<T>.create { [unowned self] observer in
-            let request = apiRequest.request(with: self.baseURL)
-            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                do {
-                    let model: T = try JSONDecoder().decode(T.self, from: data ?? Data())
-                    observer.onNext(model)
-                } catch let error {
-                    observer.onError(error)
-                }
-                observer.onCompleted()
+        let request = apiRequest.request(with: baseURL)
+        return URLSession.shared.rx.data(request: request)
+            .map {
+                try JSONDecoder().decode(T.self, from: $0)
             }
-            task.resume()
-            
-            return Disposables.create {
-                task.cancel()
-            }
-        }
     }
 }
